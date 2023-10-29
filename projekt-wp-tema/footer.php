8e5543823550c73cc854dc37f97b308d6a92835d
Louise Lindberg <123017290+loli2204@users.noreply.github.com>
2023-10-29 17:17:58 +0100

    Add files via upload

---
+++ projekt-wp-tema/footer.php
@@ -0,0 +1,19 @@
+<footer class="footer">
+    <div class="footer-content">
+        <div class="footer-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/footer-logo.png" alt="Hotel Bali Logo">
+        </div>
+        <div class="footer-links">
+            <ul>
+                <li><a href="<?php echo esc_url(home_url('/')); ?>">Hotel Bali</a></li>
+            </ul>
+        </div>
+        <div class="footer-social">
+            <ul>
+                <li><a href="#"><i class="fa-brands fa-facebook" style="color: #ffffff;"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-twitter" style="color: #ffffff;"></i></a></li>
+                <li><a href="#"><i class="fa-brands fa-instagram" style="color: #ffffff;"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
